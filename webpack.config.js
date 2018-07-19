c5bc6b7db8d4685be866d76b2fe139612a393e8f
Matsor Browncoat <prunkstation@gmail.com>
2018-07-19 07:10:58 +0200

    🥃 200.transpile

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html?$/,
+        use: [
+          {
+            loader: 'raw-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff2?)$/,
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node-modules/,
+        ],
+        use: [
+          {
+            loader: 'ng-annotate-loader',
+          },
+          {
+            loader: 'babel-loader',
+          },
+        ]
+      }
+    ],
+  },
+}
